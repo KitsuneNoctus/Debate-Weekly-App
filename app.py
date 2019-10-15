61dc0feea7b3d6dc3feb03c31df1e425dc831577
KitsuneNoctus <theceintiant@gmail.com>
2019-10-15 16:36:01 -0700

    Created the homepage for the site. Does nothing for now

--- app.py
+++ app.py
@@ -3,10 +3,23 @@ from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, url_for
 from bson.objectid import ObjectId
 import os
-
 #Project Name: Debate Weekly
+client = MongoClient()
+#db = client.get_default_database()
+db = client.Arguments
+arguments = db.arguments
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Homepage"""
+    return render_template('debate_home.html')
 
 #Create
 #Read
 #Update
 #Destroy
+if __name__=='__main__':
+    app.run()
