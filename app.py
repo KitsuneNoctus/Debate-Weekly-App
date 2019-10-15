958e5c95f1c4e1fdd7d32e534636cecf2dc09d1b
KitsuneNoctus <theceintiant@gmail.com>
2019-10-15 15:54:46 -0700

    adding rest of current files

---
+++ app.py
@@ -0,0 +1,12 @@
+#Fall 2019 Intensive 1.1 - Henry Calderon
+from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
+import os
+
+#Project Name: Debate Weekly
+
+#Create
+#Read
+#Update
+#Destroy
